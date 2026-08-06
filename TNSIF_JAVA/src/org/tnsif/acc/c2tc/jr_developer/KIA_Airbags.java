e5ac99aae0ed775f286d92c72f921595413ea3c3
Sathish R <sathistake19@gmail.com>
2026-08-06 15:34:50 +0530

    First Commit

--- TNSIF_JAVA/src/org/tnsif/acc/c2tc/jr_developer/KIA_Airbags.java
+++ TNSIF_JAVA/src/org/tnsif/acc/c2tc/jr_developer/KIA_Airbags.java
@@ -1,16 +1,8 @@
 package org.tnsif.acc.c2tc.jr_developer;
 
-public class KIA_Airbags {
-	
-	public void disp_airbags()
-	{
-		System.out.println("All KIA have Airbag 2");
-	}
-	
-	 public static void main(String[] args) {
-		 KIA_Airbags bag=new KIA_Airbags();
-		 bag.disp_airbags();
-		 
-}
+public class KIA_Airbags extends KIA_Speedlimit {
 
+    void disp_airbags() {
+        System.out.println("All KIA have Airbag 2");
+    }
 }
