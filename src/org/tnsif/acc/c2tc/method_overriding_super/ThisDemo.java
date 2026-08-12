837518adec8bc466b0161a8a46acbbd8a088a35b
Sathish R <sathistake19@gmail.com>
2026-08-12 14:45:56 +0530

    Initial TNSIF Java Training

---
+++ src/org/tnsif/acc/c2tc/method_overriding_super/ThisDemo.java
@@ -0,0 +1,42 @@
+package org.tnsif.acc.c2tc.method_overriding_super;
+
+class Person
+{
+	private String name;
+	private int age;
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	void displayDetails()
+	{
+		System.out.println("Name :"+name);
+		System.out.println("Age :"+age);
+	}
+}
+public class ThisDemo {
+
+	public static void main(String[] args) {
+		Person person=new Person();
+		person.setName("Hema");
+		person.setAge(26);
+		person.getName();
+		person.getAge();
+		person.displayDetails();
+		
+
+	}
+
+}
